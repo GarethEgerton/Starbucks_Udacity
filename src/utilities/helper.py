07e992f1d772591222bba64748a5aae8b4f534dd
garethegerton <gareth.egerton@gmail.com>
2019-10-18 16:57:23 +0100

    updating local... frustrating

---
+++ src/utilities/helper.py
@@ -0,0 +1,29 @@
+def pull():
+  '''
+  Mounts Google Drive
+  Moves to Colab Notebook director
+  Trys to download repo from Github
+  If already exists pulls and rebases
+  '''
+
+  #from google.colab import drive
+  #drive.mount('/content/drive/')
+  print('''
+  #!git clone https://GarethEgerton:P400dellpc@github.com/GarethEgerton/Starbucks_Udacity.git
+  !git config --global user.email 'gareth.egerton@gmail.com'
+  !git config --global use.name 'GarethEgerton'
+  !git pull --rebase origin master
+  ''')
+
+def push():
+  '''
+  Adds all changes to staging area
+  Commits
+  Pushes to remote repo
+  '''
+
+  print('''
+  !git add --all
+  !git commit -m message
+  !git push origin master
+  ''')
